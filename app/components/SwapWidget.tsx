e16c0b6a76b5dfe3254085c135ca1b80fc07697e
m <13090422+marciob@users.noreply.github.com>
2025-08-03 00:05:21 -0300

    adding react icons

--- app/components/SwapWidget.tsx
+++ app/components/SwapWidget.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import { IoMdSettings } from "react-icons/io"; // <-- 1. ADD THIS IMPORT
 import SettingsModal from "./SettingsModal";
 import Action from "./Action";
 import Receive from "./Receive";
@@ -61,21 +62,8 @@ export default function SwapWidget() {
                   onClick={() => setSettingsOpen(true)}
                   className="text-gray-400 hover:text-white"
                 >
-                  {/* --- NEW SETTINGS ICON SVG --- */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <circle cx="12" cy="12" r="3"></circle>
-                    <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 16.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 5a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
-                  </svg>
+                  {/* --- 2. REPLACE THE SVG WITH THE NEW ICON COMPONENT --- */}
+                  <IoMdSettings className="h-5 w-5" />
                 </button>
               </div>
               <div className="space-y-2">
